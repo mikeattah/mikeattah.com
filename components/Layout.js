c6af1d5d69494ae2686da676e1467794122df54e
Mike Attah <mailenemona@gmail.com>
2021-08-11 17:33:23 +0100

    predeployment commit 005

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   return (
     <div className={styles.container}>
       <Navbar />
-      {children}
+      <main>{children}</main>
       <Footer />
     </div>
   );
