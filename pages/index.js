36ed871278eee4f02a6b1d5da9e862ed2fc26022
Mike Attah <mailenemona@gmail.com>
2021-09-30 17:36:42 +0100

    updated home, footer, layout, and navbar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
-// import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import Layout from "../components/layout.js";
 
 export default function Home({ title, description, ...props }) {
-  // const honor = `https://img.shields.io/badge/Codewars-${props.honor}-blue?logo=codewars`;
   return (
     <Layout pageTitle={title} pageDescription={description}>
       <div className={styles.container}>
@@ -12,47 +10,20 @@ export default function Home({ title, description, ...props }) {
             Hi I&apos;m <span>Mike!</span>
           </h1>
           <h3 className={styles.description}>I am a frontend developer</h3>
-          {/* I build web applications using React, Next.js, and Gatsby. */}
-          {/* I build web, mobile and desktop apps. */}
+          {/* I build web [] mobile [] and desktop [] apps with JavaScript and TypeScript. */}
         </main>
-        {/* <div className={styles.codewars}>
-          <a
-            href="https://www.codewars.com/users/mikeattah"
-            className={styles.codewarslogo}
-          >
-            <Image
-              src={honor}
-              alt="Codewars Honor"
-              className={styles.codewarstext}
-              title="Codewars Honor"
-              layout="fill"
-              objectFit="contain"
-            />
-          </a>
-        </div> */}
       </div>
     </Layout>
   );
 }
 
-export async function getServerSideProps(context) {
-  const [configRes, codewarsRes] = await Promise.all([
-    import(`../siteconfig.json`),
-    fetch("https://www.codewars.com/api/v1/users/mikeattah"),
-  ]);
-
-  const [configData, codewarsData] = await Promise.all([
-    configRes.default,
-    codewarsRes.json(),
-  ]);
-
-  // console.log(codewarsData);
+export async function getStaticProps() {
+  const configData = await import(`../siteconfig.json`);
 
   return {
     props: {
-      title: configData.title,
-      description: configData.description,
-      honor: codewarsData.honor,
+      title: configData.default.title,
+      description: configData.default.description,
     },
   };
 }
